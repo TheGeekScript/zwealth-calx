d6ee6ca9d45dc82d4b518a8450007d09910c9761
thegeekscript <web.chaitra@gmail.com>
2025-04-01 17:39:20 +0530

    common form

--- index.php
+++ index.php
@@ -17,6 +17,13 @@
   </style>
 </head>
 <body>
+  <nav class="navbar navbar-light bg-light ">
+    <div class="container-fluid justify-content-center">
+      <a class="navbar-brand" href="#">
+        <img src="img/logo.png" alt="ZWealth Logo" width="300" class="mx-auto d-inline-block align-text-top">
+      </a>
+    </div>
+  </nav>
 
   <div class="container mt-5">
 
@@ -59,19 +66,31 @@
                     <div class="row">
                       <div class="col">
                         <label for="currentAge" class="form-label">Current Age (Years)</label>
-                        <input type="number" class="form-control" id="currentAge" name="currentAge" required value="27">
+                        <input type="number" class="form-control" id="currentAge" name="currentAge" required value="35">
                       </div>
                       <div class="col">
-                        <label for="retirementAge" class="form-label">Desired Retirement Age (Years)</label>
-                        <input type="number" class="form-control" id="retirementAge" name="retirementAge" required value="60">
+                        <label for="monthlyIncomeRequired" class="form-label">Monthly Expenses (₹)</label>
+                        <input type="number" class="form-control" id="monthlyIncomeRequired" name="monthlyIncomeRequired" required value="35000">
                       </div>
                     </div>
                   </div>
                   <div class="mb-3">
                     <div class="row">
                       <div class="col">
+                        <label for="retirementAge" class="form-label">Retirement Age (Years)</label>
+                        <input type="number" class="form-control" id="retirementAge" name="retirementAge" required value="60">
+                      </div>
+                      <div class="col">
                         <label for="lifeExpectancy" class="form-label">Life Expectancy (Years)</label>
                         <input type="number" class="form-control" id="lifeExpectancy" name="lifeExpectancy" required value="80">
+                      </div>
+                    </div>
+                  </div>
+                  <div class="mb-3">
+                    <div class="row">
+                      <div class="col">
+                        <label for="existingRetirementFund" class="form-label">Existing Retirement Fund (₹)</label>
+                        <input type="number" class="form-control" id="existingRetirementFund" name="existingRetirementFund" required value="10000">
                       </div>
                       <div class="col">
                         <label for="inflationRate" class="form-label">Expected Inflation Rate (%)</label>
@@ -80,14 +99,10 @@
                     </div>
                   </div>
                   <div class="mb-3">
-                      <label for="monthlyIncomeRequired" class="form-label">Monthly Income Required in Retirement Years (₹)</label>
-                      <input type="number" class="form-control" id="monthlyIncomeRequired" name="monthlyIncomeRequired" required value="100000">
-                  </div>
-                  <div class="mb-3">
                     <div class="row">
                       <div class="col">
                         <label for="preRetirementReturn" class="form-label">Expected Return on Investment (Pre-retirement) (%)</label>
-                        <input type="number" class="form-control" id="preRetirementReturn" name="preRetirementReturn" required value="12">
+                        <input type="number" class="form-control" id="preRetirementReturn" name="preRetirementReturn" required value="15">
                       </div>
                       <div class="col">
                         <label for="postRetirementReturn" class="form-label">Expected Return on Investment (Post-retirement) (%)</label>
@@ -95,11 +110,7 @@
                       </div>
                     </div>
                   </div>
-                  <div class="mb-3">
-                      <label for="existingRetirementFund" class="form-label">Existing Retirement Fund (₹)</label>
-                      <input type="number" class="form-control" id="existingRetirementFund" name="existingRetirementFund" required value="100000">
-                  </div>
-                  <button type="submit" class="btn btn-primary">Calculate</button>
+                  <button type="submit" class="btn btn-primary text-uppercase">Calculate</button>
                 </form>
               </div>
               <div class="col-md-5">
@@ -461,6 +472,52 @@
     </div>
   </div>
 
+  <div class="container mt-5">
+      <div class="row col-md-12 justify-content-center">
+				<img src="img/banner2.png" alt="" class="mx-auto img-responsive">
+			</div>
+    <form id="infoform" method="POST" class="row bg-light p-4">
+        <div class="col-md-3">
+            <input type="text" class="form-control" id="infoform_name" name="name" placeholder="Name" required>
+        </div>
+        <div class="col-md-3">
+            <input type="email" class="form-control" id="infoform_email" name="email" placeholder="Email" required>
+        </div>
+        <div class="col-md-3">
+            <input type="tel" class="form-control" id="infoform_phone" name="phone" placeholder="Phone" required>
+        </div>
+        <div class="col-md-3">
+            <button type="submit" class="btn btn-primary mb-3 text-uppercase">I'm ready to invest</button>
+        </div>
+    </form>
+    <div id="infoform_message" class="mt-3 pt-3"></div>
+  </div>
+
+  <div class="container-fluid">
+    <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
+      <div class="col-md-6 d-flex align-items-center">
+        <span class="text-muted">© 2024 - <?=date('Y');?> ZWealth. All Rights Reserved.</span>
+      </div>
+
+      <ul class="nav col-md-6 justify-content-end list-unstyled d-flex">
+        <li class="ms-3">
+          <a class="text-muted" target="_blank" href="https://www.facebook.com/profile.php?id=61566605033208&mibextid=LQQJ4d">
+            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-facebook" viewBox="0 0 16 16">
+            <path d="M16 8.049c0-4.446-3.582-8.05-8-8.05C3.58 0-.002 3.603-.002 8.05c0 4.017 2.926 7.347 6.75 7.951v-5.625h-2.03V8.05H6.75V6.275c0-2.017 1.195-3.131 3.022-3.131.876 0 1.791.157 1.791.157v1.98h-1.009c-.993 0-1.303.621-1.303 1.258v1.51h2.218l-.354 2.326H9.25V16c3.824-.604 6.75-3.934 6.75-7.951"/>
+            </svg>
+          </a>
+        </li>
+        <li class="ms-3">
+          <a class="text-muted" target="_blank" href="https://www.instagram.com/zwealth.in">
+            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-instagram" viewBox="0 0 16 16">
+            <path d="M8 0C5.829 0 5.556.01 4.703.048 3.85.088 3.269.222 2.76.42a3.9 3.9 0 0 0-1.417.923A3.9 3.9 0 0 0 .42 2.76C.222 3.268.087 3.85.048 4.7.01 5.555 0 5.827 0 8.001c0 2.172.01 2.444.048 3.297.04.852.174 1.433.372 1.942.205.526.478.972.923 1.417.444.445.89.719 1.416.923.51.198 1.09.333 1.942.372C5.555 15.99 5.827 16 8 16s2.444-.01 3.298-.048c.851-.04 1.434-.174 1.943-.372a3.9 3.9 0 0 0 1.416-.923c.445-.445.718-.891.923-1.417.197-.509.332-1.09.372-1.942C15.99 10.445 16 10.173 16 8s-.01-2.445-.048-3.299c-.04-.851-.175-1.433-.372-1.941a3.9 3.9 0 0 0-.923-1.417A3.9 3.9 0 0 0 13.24.42c-.51-.198-1.092-.333-1.943-.372C10.443.01 10.172 0 7.998 0zm-.717 1.442h.718c2.136 0 2.389.007 3.232.046.78.035 1.204.166 1.486.275.373.145.64.319.92.599s.453.546.598.92c.11.281.24.705.275 1.485.039.843.047 1.096.047 3.231s-.008 2.389-.047 3.232c-.035.78-.166 1.203-.275 1.485a2.5 2.5 0 0 1-.599.919c-.28.28-.546.453-.92.598-.28.11-.704.24-1.485.276-.843.038-1.096.047-3.232.047s-2.39-.009-3.233-.047c-.78-.036-1.203-.166-1.485-.276a2.5 2.5 0 0 1-.92-.598 2.5 2.5 0 0 1-.6-.92c-.109-.281-.24-.705-.275-1.485-.038-.843-.046-1.096-.046-3.233s.008-2.388.046-3.231c.036-.78.166-1.204.276-1.486.145-.373.319-.64.599-.92s.546-.453.92-.598c.282-.11.705-.24 1.485-.276.738-.034 1.024-.044 2.515-.045zm4.988 1.328a.96.96 0 1 0 0 1.92.96.96 0 0 0 0-1.92m-4.27 1.122a4.109 4.109 0 1 0 0 8.217 4.109 4.109 0 0 0 0-8.217m0 1.441a2.667 2.667 0 1 1 0 5.334 2.667 2.667 0 0 1 0-5.334"/>
+            </svg>
+          </a>
+        </li>
+      </ul>
+    </footer>
+  </div>
+
   <script>
 
   // retirement cal
@@ -481,22 +538,22 @@
     const yearsToRetirement = retirementAge - currentAge;
     const annualIncomeRequired = monthlyIncomeRequired * 12 * Math.pow(1 + inflationRate, yearsToRetirement);
 
-    // Step 2: Calculate Total Corpus Required for After Retirement
+    // Step 2: Calculate Additional Income Required for Retirement
     const retirementYears = lifeExpectancy - retirementAge;
-    const totalCorpusRequired = annualIncomeRequired * ((1 - Math.pow(1 + postRetirementReturn, -retirementYears)) / postRetirementReturn);
+    const additionalIncomeRequired = annualIncomeRequired * ((1 - Math.pow(1 + postRetirementReturn, -retirementYears)) / postRetirementReturn);
 
-    // Step 3: Calculate Monthly Savings Required to Accumulate the Corpus
+    // Step 3: Calculate Monthly Savings Required to Retire
     const futureValueOfExistingFund = existingRetirementFund * Math.pow(1 + preRetirementReturn, yearsToRetirement);
     const monthlyRate = preRetirementReturn / 12;
     const totalPayments = yearsToRetirement * 12;
-    const monthlySavingsRequired = (totalCorpusRequired - futureValueOfExistingFund) * (monthlyRate / (Math.pow(1 + monthlyRate, totalPayments) - 1));
+    const monthlySavingsRequired = (additionalIncomeRequired - futureValueOfExistingFund) * (monthlyRate / (Math.pow(1 + monthlyRate, totalPayments) - 1));
 
     // Format numbers with commas for better readability
     const formatNumber = (num) => num.toLocaleString('en-IN', { maximumFractionDigits: 0 });
 
     // Display results
     document.getElementById('annualIncomeRequired').innerText = `₹${formatNumber(annualIncomeRequired)}`;
-    document.getElementById('totalCorpusRequired').innerText = `₹${formatNumber(totalCorpusRequired)}`;
+    document.getElementById('totalCorpusRequired').innerText = `₹${formatNumber(additionalIncomeRequired)}`;
     document.getElementById('monthlySavingsRequired').innerText = `₹${formatNumber(monthlySavingsRequired)}`;
 
     // Prepare email data as an array/object
@@ -871,6 +928,29 @@
       } catch (error) {
           console.error('Error:', error);
           document.getElementById('swp_message').innerHTML = '<div class="alert alert-danger alert-dismissible fade show">An error occurred. Please try again.<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button></div>';
+      }
+    });
+
+    document.getElementById('infoform').addEventListener('submit', async function (event) {
+      event.preventDefault();
+
+      const infoform_name = document.getElementById('infoform_name').value;
+      const infoform_email = document.getElementById('infoform_email').value;
+      const infoform_phone = document.getElementById('infoform_phone').value;
+      try {
+          const response = await fetch('mails/sendform.php', {
+              method: 'POST',
+              headers: {
+                  'Content-Type': 'application/x-www-form-urlencoded',
+              },
+              body: `infoform_name=${encodeURIComponent(infoform_name)}&infoform_email=${encodeURIComponent(infoform_email)}&infoform_phone=${encodeURIComponent(infoform_phone)}`,
+          });
+
+          const result = await response.text();
+          document.getElementById('infoform_message').innerHTML = result;
+      } catch (error) {
+          console.error('Error:', error);
+          document.getElementById('infoform_message').innerHTML = '<div class="alert alert-danger alert-dismissible fade show">An error occurred. Please try again.<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button></div>';
       }
     });
 </script>
